4354e1d34c974d4b3da2b2d9d4d8693ed2ecc551
IshanKM <ishanmadushanka.sg@gmail.com>
2024-09-14 01:35:42 -0700

    first commit

---
+++ ios-test/Shared/ios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_testApp.swift
+//  Shared
+//
+//  Created by IKM on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
